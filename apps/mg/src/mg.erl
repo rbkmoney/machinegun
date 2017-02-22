b75746365c1ed0f57e81e96a6a3bc3d8b6961e04
Petr Kozorezov <petr.kozorezov@gmail.com>
2017-02-22 15:43:51 +0300

    MG-66: refactor MG interface: use "msgpack:Value" instead of "binary"

--- apps/mg/src/mg.erl
+++ apps/mg/src/mg.erl
@@ -5,6 +5,7 @@
 -export_type([ns       /0]).
 -export_type([id       /0]).
 
--type opaque() :: null | true | false | number() | binary() | [opaque()] | #{opaque() => opaque()}.
+% it's a copy from msgpack_term()
+-type opaque() :: null | true | false | number() | binary() | {string, string()} | [opaque()] | #{opaque() => opaque()}.
 -type ns    () :: binary().
 -type id    () :: binary().
