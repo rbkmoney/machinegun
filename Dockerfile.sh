769bd38b57bb3230fded6acd76b2c5a672745e9a
Petr Kozorezov <petr.kozorezov@gmail.com>
2017-11-01 17:43:42 +0300

    bump build and base images (#91)
    * bump build and base images
    
    * remove distclean deps from release building (blyad')
    
    * optimize dockerfile

--- Dockerfile.sh
+++ Dockerfile.sh
@@ -22,19 +22,21 @@ WORKDIR /opt/machinegun
 ENTRYPOINT ./bin/entrypoint.sh
 CMD ./etc/config.yaml
 EXPOSE 8022
-LABEL base_image_tag=$BASE_IMAGE_TAG
-LABEL build_image_tag=$BUILD_IMAGE_TAG
-# A bit of magic to get a proper branch name
+# A bit of magic below to get a proper branch name
 # even when the HEAD is detached (Hey Jenkins!
 # BRANCH_NAME is available in Jenkins env).
-LABEL branch=$( \
-  if [ "HEAD" != $(git rev-parse --abbrev-ref HEAD) ]; then \
-    echo $(git rev-parse --abbrev-ref HEAD); \
-  elif [ -n "$BRANCH_NAME" ]; then \
-    echo $BRANCH_NAME; \
-  else \
-    echo $(git name-rev --name-only HEAD); \
-  fi)
-LABEL commit=$(git rev-parse HEAD)
-LABEL commit_number=$(git rev-list --count HEAD)
+LABEL com.rbkmoney.$SERVICE_NAME.parent=$BASE_IMAGE_NAME \
+      com.rbkmoney.$SERVICE_NAME.parent_tag=$BASE_IMAGE_TAG \
+      com.rbkmoney.$SERVICE_NAME.build_img=build \
+      com.rbkmoney.$SERVICE_NAME.build_img_tag=$BUILD_IMAGE_TAG \
+      com.rbkmoney.$SERVICE_NAME.commit_id=$(git rev-parse HEAD) \
+      com.rbkmoney.$SERVICE_NAME.commit_number=$(git rev-list --count HEAD) \
+      com.rbkmoney.$SERVICE_NAME.branch=$( \
+        if [ "HEAD" != $(git rev-parse --abbrev-ref HEAD) ]; then \
+          echo $(git rev-parse --abbrev-ref HEAD); \
+        elif [ -n "$BRANCH_NAME" ]; then \
+          echo $BRANCH_NAME; \
+        else \
+          echo $(git name-rev --name-only HEAD); \
+        fi)
 EOF
