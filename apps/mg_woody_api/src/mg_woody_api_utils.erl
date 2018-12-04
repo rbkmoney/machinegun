6682caaadad267fc9623b43376258d1c7896f7db
Evgeny Levenets <levenets.e.e@gmail.com>
2018-12-04 13:38:03 +0300

    MG-139: remove retries from woody_handler

--- apps/mg_woody_api/src/mg_woody_api_utils.erl
+++ apps/mg_woody_api/src/mg_woody_api_utils.erl
@@ -96,9 +96,11 @@ handle_error({logic, {invalid_machine_id, _}}, _) -> {rethrow, #mg_stateproc_Mac
 
 % может Reason не прокидывать дальше?
 % TODO logs
-handle_error({transient, _     }, {wait, Timeout, NewStrategy}) -> {retry_in, Timeout, NewStrategy};
-handle_error({transient, Reason}, finish                      ) -> {woody_error, {resource_unavailable, Reason}};
-handle_error(Reason={timeout, _}, _                           ) -> {woody_error, {result_unknown      , Reason}};
+% повторы запросов только для стораджа, остальные не заслужили
+handle_error({transient, {storage_unavailable, _Reason}}, {wait, Timeout, NewStrategy}) -> {retry_in, Timeout, NewStrategy};
+
+handle_error({transient, Reason}, _) -> {woody_error, {resource_unavailable, Reason}};
+handle_error(Reason={timeout, _}, _) -> {woody_error, {result_unknown      , Reason}};
 
 handle_error(UnknownError, _) -> erlang:error(badarg, [UnknownError]).
 
