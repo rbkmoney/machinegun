00aa3098226e103a1a3626b3edf63864b94c4036
Andrey Fadeev <me@ciiol.net>
2019-09-24 17:05:30 +0300

    MG-171 Fix typo in configurator (#184)
    Also add configurator test to CI pipeline.
    And do not silence configurator output on startup.

--- rel_scripts/entrypoint.sh
+++ rel_scripts/entrypoint.sh
@@ -17,8 +17,8 @@
 
 set -e
 
-ROOT="$(dirname $0)/.."
+ROOT="$(realpath $(dirname $0)/..)"
 RELEASE_DIR="${ROOT}/releases/{{release_version}}"
 YAML_CONFIG=${1:-${ROOT}/etc/config.yaml}
-ERL_LIBS="${ROOT}/lib" ${ROOT}/erts-{{release_erts_version}}/bin/escript ${ROOT}/bin/configurator.escript ${YAML_CONFIG} ${RELEASE_DIR} > /dev/null
+ERL_LIBS="${ROOT}/lib" ${ROOT}/erts-{{release_erts_version}}/bin/escript ${ROOT}/bin/configurator.escript ${YAML_CONFIG} ${RELEASE_DIR}
 exec ${ROOT}/bin/machinegun foreground
