ab6a2fe54168650e6c511cc9922282e8b7b9139a
Andrew Mayorov <a.mayorov@rbkmoney.com>
2019-10-08 22:02:54 +0300

    [wip] Trace manager inbox

--- apps/mg/test/mg_workers_SUITE.erl
+++ apps/mg/test/mg_workers_SUITE.erl
@@ -398,7 +398,10 @@ start_workers(Options) ->
 -spec stop_workers(pid()) ->
     ok.
 stop_workers(Pid) ->
-    ok = proc_lib:stop(Pid, normal, 5000),
+    ct:pal("Manager inbox: ~p", [erlang:process_info(Pid, messages)]),
+    ok = try proc_lib:stop(Pid, normal, 5000) after
+        ct:pal("Manager inbox: ~p", [erlang:process_info(Pid, messages)])
+    end,
     ok.
 
 -spec wait_machines_unload(pos_integer()) ->
