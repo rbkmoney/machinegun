678859ee0d15a43a17f1840171d26a045d6b4826
Igor Toporkov <i.toporkov@rbkmoney.com>
2020-02-06 12:15:28 +0300

    Initial

---
+++ src/machinegun.erl
@@ -0,0 +1,36 @@
+-module(machinegun).
+
+%% API
+-export([start/0]).
+-export([stop /0]).
+
+%% application callbacks
+-behaviour(application).
+-export([start/2]).
+-export([stop /1]).
+
+-spec start() ->
+    {ok, _}.
+start() ->
+    application:ensure_all_started(?MODULE).
+
+-spec stop() ->
+    ok.
+stop() ->
+    application:stop(?MODULE).
+
+-spec start(_, _) ->
+    {ok, pid()}.
+start(_StartType, _StartArgs) ->
+    Config = genlib_app:env(?MODULE),
+    ChildSpecs = mg_woody_api:child_specs(Config),
+    mg_utils_supervisor_wrapper:start_link(
+        {local, ?MODULE},
+        #{strategy => rest_for_one},
+        ChildSpecs
+    ).
+
+-spec stop(any()) ->
+    ok.
+stop(_State) ->
+    ok.
